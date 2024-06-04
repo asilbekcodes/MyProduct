58cebf71e6fc2895537c4e0187f00f8a640fff71
Asilbek Codes <asilbeksuvonov43@gmail.com>
2024-06-04 15:47:44 +0500

    section-first

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'section-1': "url('/assets/imgs/Bg.svg')",
+      },
+      colors:{
+        'text-clr': "#22343D",
+        'green-clr': "#02897A"
+      },
+    },
   },
   plugins: [],
 }

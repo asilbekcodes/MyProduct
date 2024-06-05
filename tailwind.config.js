e5bebc9da75477a4747661dee42212d5dd5d7d0f
Asilbek Codes <asilbeksuvonov43@gmail.com>
2024-06-05 11:35:49 +0500

    tailwind config.js ga bg added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         'section-1': "url('/assets/imgs/Bg.svg')",
+        'section-5': "url('/assets/imgs/Bg (1).svg')"
       },
       colors:{
         'text-clr': "#22343D",

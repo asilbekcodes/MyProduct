a177cd29ef51e5396f99cb2d4bea0d7ba2f1e2b0
Asilbek Codes <asilbeksuvonov43@gmail.com>
2024-06-10 14:59:36 +0500

    login add

--- script/script.js
+++ script/script.js
@@ -8,3 +8,40 @@ document.getElementById('toggleModeButton').addEventListener('click', function()
         body.classList.add('day-mode');
     }
 });
+
+
+let button = document.getElementById('openButton')
+
+let span = document.getElementById('close')
+
+let modal = document.getElementById('items') 
+
+button.onclick = function(){
+    modal.style.display = 'block';
+}
+
+span.onclick = function () {
+    modal.style.display = 'none';
+}
+
+
+
+
+let btn = document.getElementById('btn')
+btn.addEventListener('click', function(){
+    let a = document.getElementById('email').value;
+    let border = document.getElementById('email')
+    let b = document.getElementById('password').value;
+    let border2 = document.getElementById('password')
+
+    if (a === '') {
+        border.style.borderColor = 'red'
+    } else if (b === '') {
+        border2.style.borderColor = 'red'
+    }else{
+        border.style.borderColor = 'green'
+        border2.style.borderColor = 'green'
+        window.location.href = 'pricing.html'
+    }
+
+})
